1e51aab9d3b61dd517ad832a25b551963c1e01b5
Juan Aguillon <carlosaguillon@Juan-Aguillons-iMac.local>
2017-05-06 18:21:47 -0700

    Initial Commit

---
+++ CurrencyExchange/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrencyExchange
+//
+//  Created by Juan Aguillon on 5/6/17.
+//  Copyright © 2017 Juan Aguillon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
