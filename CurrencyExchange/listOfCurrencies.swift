30a49a877da32dbe6ecda84efd51b53d5469208c
CampusUser <campususer@csufmac-031.local>
2017-05-09 21:50:42 -0700

    Added currencies to table view dynamically

--- CurrencyExchange/listOfCurrencies.swift
+++ CurrencyExchange/listOfCurrencies.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+import UIKit
+
+class listOfCurrencies: UITableViewController {
+    
+    var currencyList: [String] = []
+    
+    override func viewDidLoad() {
+        
+        currencyList=["US Dollar","Mexican Peso","Japanese Yen","British Pound","Canadian Dollar","European Union Euro","Chinese Yuan"]
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currencyList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text=currencyList[indexPath.row]
+        
+        return cell
+    }
+}
